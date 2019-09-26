d4c01de22efeca951c92f3c3dc0c6c71fd4b0a9b
omatviiv <oleh.matviiv@gmail.com>
2019-09-26 10:17:41 +0300

    master - add clone.sh script

---
+++ README-COMPONENT.md
@@ -0,0 +1,16 @@
+Component description
+
+# Install
+```
+npm i your-component-name
+```
+
+# Use cases
+Show how to use your component
+```
+<MyComponent data={[...]}>Children</MyComponent>
+```
+
+# Start component development
+1. Start webpack dev server for demo project `npm run demo`.
+2. Develop your component.
---
+++ clone.sh
@@ -0,0 +1,18 @@
+echo cloning new component: $1
+echo ==copy demo folder
+cp -rf ./demo $1/
+echo ==copy component folder
+cp -rf ./component $1/
+echo ==copy .gitignore
+cp ./.gitignore $1/
+echo ==copy README.md
+cp ./README-COMPONENT.md $1/README.md
+echo ==copy RELEASES.md
+cp ./RELEASES.md $1/
+echo ==copy todo
+cp ./todo-component $1/todo
+echo ==copy webpack.config.js
+cp ./webpack.config.js $1/
+echo ==copy package.json
+cp ./package.json $1/
+echo "==DON'T forget to update package.json with new component related data"
